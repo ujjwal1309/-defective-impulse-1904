7b6defb2a1d9daac09ab5aee32d3c850c74e0e2a
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-09-28 23:48:17 +0530

    Skincare Day-2

---
+++ navbar.js
@@ -0,0 +1,43 @@
+
+var navbar=document.querySelector(".navbar");
+var help=document.querySelector(".help")
+
+window.onscroll=function(){
+    if(window.pageYOffset >= help.offsetTop)
+    {
+        navbar.classList.add("sticky")
+    }
+    else
+    {
+        navbar.classList.remove("sticky")
+    }
+}
+
+
+document.querySelector("#search").addEventListener("mouseover",blur);
+document.querySelector("#search").addEventListener("mouseout",blurNormal);
+
+function blur(){
+    var search=document.getElementsByTagName("body")[0];
+    search.style.background="rgba(0,0,0,0.3 )"
+}
+function blurNormal(){
+    var search=document.getElementsByTagName("body")[0];
+    search.style.background="none";
+}
+
+document.querySelector("#brand").addEventListener("mouseover",brand);
+document.querySelector("#brand").addEventListener("mouseout",brandClose);
+
+
+function brand(){
+    document.querySelector(".brands").classList.add("brands-show");
+    var search=document.getElementsByTagName("body")[0];
+    search.style.background="rgba(0,0,0,0.3 )"
+}
+
+function brandClose(){
+    document.querySelector(".brands").classList.remove("brands-show");
+    var search=document.getElementsByTagName("body")[0];
+    search.style.background="none";
+}
